e68d3b2e70301374cca07bf10e4ee38a652da881
Damian Bruera <brueradamian@gmail.com>
2022-05-30 16:15:43 -0300

    feat: enable mongoose db connection

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
+const dbUrl = process.env.DB_URL || '';
 
 app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server listening on port: ${port}`);
+mongoose.connect(dbUrl, (err, re) => {
+  if (err) throw err;
+  console.log(`Connection Established at: ${dbUrl}`);
+
+  app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+  });
 });
