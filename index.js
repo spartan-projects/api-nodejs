c48f0a770b653690d642076cfd0aa581a0c82d46
Damian Bruera <brueradamian@gmail.com>
2022-06-03 18:16:23 -0300

    refactor: refactor product routes and dynamic load routes support

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const productRoutes = require('./src/routes/product');
+const productRoutes = require('./src/routes');
 const mongoDb = require('./src/config/mongo');
 
 const port = process.env.PORT || 3000;
@@ -16,6 +16,12 @@ app.use(
 );
 app.use(express.json());
 
+/*
+TODO:
+  productRoutes require will take automatically
+  the index.js file in src/routes folder.
+  index.js adds dynamically routes to the express.Router.
+*/
 app.use('/api/v1/', productRoutes);
 
 app.listen(port, () => {
