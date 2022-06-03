db5845892d0e543a749b61b91a3f724bf82f98ee
Damian Bruera <brueradamian@gmail.com>
2022-06-03 16:10:50 -0300

    refactor: refactor scaffolding

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
 const productRoutes = require('./src/routes/product');
+const mongoDb = require('./src/config/mongo');
 
 const port = process.env.PORT || 3000;
-const dbUrl = process.env.DB_URL || '';
 
 app.use(cors());
 app.use(
@@ -19,11 +18,8 @@ app.use(express.json());
 
 app.use('/api/v1/', productRoutes);
 
-mongoose.connect(dbUrl, (err, re) => {
-  if (err) throw err;
-  console.log(`Connection Established at: ${dbUrl}`);
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
 
-  app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
-  });
+  mongoDb();
 });
